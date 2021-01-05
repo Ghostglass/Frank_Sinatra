5a5b57f4663bd780c51b1a7b87f219ea08c1ddd2
Kim <kthachrenouf@yahoo.com>
2021-01-05 16:30:46 +0000

    Sassy Cat

--- app.rb
+++ app.rb
@@ -8,3 +8,9 @@ end
 get '/secret' do
   "Halloumi and Lasagna"
 end
+
+get '/cat' do
+  '<div style="border:10px dotted orange;">
+  <img style="border:10px dotted gold;"src="https://i.pinimg.com/originals/e1/d0/ee/e1d0ee4a65813fe86d102185b2e8d37f.jpg">
+  </div>'
+end
